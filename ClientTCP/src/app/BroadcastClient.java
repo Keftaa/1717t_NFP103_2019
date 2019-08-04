105c778a8a2779d484e929536b0d516f25be53c4
Adnan Hamzeh <adnan.37h@gmail.com>
2019-08-04 16:00:40 +0300

    added broadcast client (not tested yet)

---
+++ ClientTCP/src/app/BroadcastClient.java
@@ -0,0 +1,26 @@
+package app;
+
+import java.io.IOException;
+import java.net.*;
+
+public class BroadcastClient implements Runnable {
+    @Override
+    public void run() {
+        try {
+            DatagramSocket c = new DatagramSocket();
+            c.setBroadcast(true);
+            byte[] sendData = "SERVER_SCAN".getBytes();
+            DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, InetAddress.getByName("192.168.0.255"), 2001);
+            c.send(sendPacket);
+
+            byte[] recBuf = new byte[15000];
+            DatagramPacket receivePacket = new DatagramPacket(recBuf, recBuf.length);
+            c.receive(receivePacket);
+
+            System.out.println("Serveur disponible: " + receivePacket.getAddress().getHostAddress());
+            c.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
--- ClientTCP/src/app/ClientTCP.java
+++ ClientTCP/src/app/ClientTCP.java
@@ -10,6 +10,7 @@ public class ClientTCP {
     private Socket clientSocket;
     private PrintWriter out;
     private BufferedReader in;
+    private boolean hasStopped = false;
 
 
 
@@ -27,12 +28,7 @@ public class ClientTCP {
         clientSocket = new Socket(ip, port);
         out = this.getOutput();
         in = this.getInput();
-        Scanner scanner = new Scanner(System.in);
-        while(!clientSocket.isClosed()) {
-            String input = scanner.nextLine();
-            this.send(input);
-        }
-        scanner.close();
+        System.out.println("Connecté!");
     }
 
     public void send(String msg) throws IOException {
@@ -41,6 +37,53 @@ public class ClientTCP {
         this.checkResponse(resp);
     }
 
+    public void sendName(String msg) {
+        out.println(msg);
+    }
+
+    public void initUserInput() throws IOException{
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("TCP client prêt");
+        System.out.println("_help pour commencer");
+        while(!hasStopped){
+            String input = scanner.nextLine();
+            if(clientSocket != null && !clientSocket.isClosed()) {
+                this.send(input);
+            }
+            this.processInput(input);
+        }
+    }
+
+    private void processInput(String input) {
+        if(input.startsWith("_fetch")){
+            System.out.println("Recherche en cours, patientez...");
+            Thread bc = new Thread(new BroadcastClient());
+            bc.start();
+            System.out.println("Recherche terminée");
+        } else if(input.startsWith("_connect")) {
+            this.connectToServer(input);
+        } else if(input.equals("_help")) {
+            this.printHelp();
+        }
+    }
+
+    private void printHelp(){
+        System.out.println("Tapez _fetch pour rechercher des serveurs");
+        System.out.println("_connect <ip serveur> <votre pseudonyme>");
+        System.out.println("_help pour ce message");
+    }
+
+    private void connectToServer(String input) {
+        String[] params = input.split(" ");
+        System.out.println("Connexion en cours...");
+        try {
+            this.start(params[1], 2000);
+            this.sendName(params[2]);
+        } catch (IOException e) {
+            System.err.println("Connexion échouée!");
+            e.printStackTrace();
+        }
+    }
     private void checkResponse(String resp){
         System.out.println(resp);
         if("bye".equals(resp)) {
@@ -55,12 +98,13 @@ public class ClientTCP {
     public void stop() throws IOException {
         in.close();
         out.close();
+        hasStopped = true;
         clientSocket.close();
     }
 
     public static void main(String[] args) throws IOException
     {
         ClientTCP client = new ClientTCP();
-        client.start("127.0.0.1", 2000);
+        client.initUserInput();
     }
 }
